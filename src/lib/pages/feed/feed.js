e711c95f306c8336c92255c903a6f297944bfbb4
deboramds <deborah.mart@hotmail.com>
2022-12-12 22:42:47 -0300

    função-de-postar

--- src/lib/pages/feed/feed.js
+++ src/lib/pages/feed/feed.js
@@ -1,38 +1,34 @@
 import { userStateLogout, userStateChanged } from '../../../configurafirebase/exports.js';
 
-
 export default () => {
   const container = document.createElement('div');
   const template = `
         <textarea id="textstory" placeholder= "escreva seu post"></textarea>
         <button id="submit">Postar</button> 
         <button id="logout">sair</button>
+        <div id="postcontainer"></div>
        `;
   container.innerHTML = template;
   const botaoSair = container.querySelector('#logout');
+  const botaoPostar = container.querySelector('#submit');
+  const textoPost = container.querySelector('#textstory');
+  const postArea = container.querySelector('#postcontainer');
+  // eslint-disable-next-line no-use-before-define
   botaoSair.addEventListener('click', deslogar);
-
   function deslogar() {
     console.log('deslogar');
-    userStateLogout(userStateChanged)
-    
-}
+    userStateLogout(userStateChanged);
+  }
 
-  
-  // const form = container.querySelector('#cadastrarRegister');
-  // const email = container.querySelector('#usernameRegister');
-  // const senha = container.querySelector('#passwordRegister');
-  // const name = container.querySelector('#nameRegister');
+  botaoPostar.addEventListener('click', postar);
+  function postar() {
+    console.log(textoPost.value);
+    postArea.innerHTML += `
+        <h1>${textoPost.value}</h1>    
+        <!--<p>${user.displayName}</p>-->
+    `
+  }
 
-  // form.addEventListener('click', (e) => {
-  //   e.preventDefault();
-  //   createUser(name.value, email.value, senha.value)
-  //     .then(() => {
-  //       window.location.hash = '#feed';
-  //     })
-  //     // eslint-disable-next-line no-unused-vars
-  //     .catch((error) => {
-  //     });
-  // });
+ 
   return container;
 };
