2f03eca615dce9a5fadd8b3b869b43ccfa53d2f3
deboramds <deborah.mart@hotmail.com>
2023-07-06 21:43:46 -0300

    correção-função-delete-post

--- src/configurafirebase/configfirestore.js
+++ src/configurafirebase/configfirestore.js
@@ -1,26 +1,45 @@
-import { collection, addDoc, getFirestore, getDocs } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js'; // eslint-disable-line import/no-unresolved
+import { collection, addDoc, getFirestore, getDocs, deleteDoc, doc } from 'https://www.gstatic.com/firebasejs/9.9.3/firebase-firestore.js';
 import { app } from './configfirebase.js';
 
-export const db = getFirestore (app);
+export const db = getFirestore(app);
 
 export const loginUser = (email, password) => {
-}
-export const createPost = async ( textoPost ) => {
-    // Add a new document with a generated id.
-    const docRef = await addDoc(collection(db, "posts"), {
-        textoPost: textoPost,
-    });
-    console.log("Document written with ID: ", docRef.id);
+  // Implementação da função loginUser
+};
 
-}
-export const getPosts = async () =>{
-    const posts = []
-    const querySnapshot = await getDocs(collection(db, "posts"));
+export const createPost = async (textoPost) => {
+  try {
+    const docRef = await addDoc(collection(db, 'posts'), {
+      textoPost: textoPost,
+    });
+    console.log('Document written with ID: ', docRef.id);
+  } catch (error) {
+    console.error('Error creating post:', error);
+  }
+};
+
+export const getPosts = async () => {
+  try {
+    const posts = [];
+    const querySnapshot = await getDocs(collection(db, 'posts'));
     querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        posts.push({id:doc.id, ...doc.data()});
+      posts.push({ id: doc.id, ...doc.data() });
     });
     return posts;
-}
+  } catch (error) {
+    console.error('Error getting posts:', error);
+    return [];
+  }
+};
+
+export const deletePost = async (postId) => {
+  try {
+    await deleteDoc(doc(db, 'posts', postId));
+    console.log('Document with ID deleted: ', postId);
+  } catch (error) {
+    console.error('Error deleting post:', error);
+  }
+};
+
 
 
--- src/lib/pages/feed/feed.js
+++ src/lib/pages/feed/feed.js
@@ -1,17 +1,16 @@
-import { createPost, getPosts } from '../../../configurafirebase/configfirestore.js';
+import { createPost, getPosts, deletePost } from '../../../configurafirebase/configfirestore.js';
 import { userStateLogout, userStateChanged } from '../../../configurafirebase/exports.js';
-
 
 export default async () => {
   const container = document.createElement('div');
   const template = `
-  <div class = "container-feed">
-    <div class = "input-post">
-      <textarea id="textstory" placeholder="escreva seu post"></textarea>
-      <button id="submit">Postar</button>
-    </div> 
-    <button id="logout">sair</button>
-    <div id="postcontainer"></div>
+    <div class="container-feed">
+      <div class="input-post">
+        <textarea id="textstory" placeholder="escreva seu post"></textarea>
+        <button id="submit">Postar</button>
+      </div> 
+      <button id="logout">sair</button>
+      <div id="postcontainer"></div>
     </div>
   `;
   container.innerHTML = template;
@@ -21,7 +20,7 @@ export default async () => {
   const postArea = container.querySelector('#postcontainer');
 
   botaoSair.addEventListener('click', deslogar);
-  function deslogar() {
+  async function deslogar() {
     console.log('deslogar');
     userStateLogout(userStateChanged);
   }
@@ -30,21 +29,42 @@ export default async () => {
   async function postar() {
     console.log(textoPost.value);
     await createPost(textoPost.value);
-    postArea.innerHTML += `
-      <h1>${textoPost.value}</h1>    
+    const newPost = document.createElement('div');
+    newPost.innerHTML = `
+      <h1>${textoPost.value}</h1>
+      <button class="delete-btn">Deletar</button>
+      <button class="like-btn">Curtir</button>
     `;
+    postArea.appendChild(newPost);
     textoPost.value = '';
   }
 
   const posts = await getPosts();
   console.log(posts);
-  for (const post of posts) {
-    postArea.innerHTML += `
-      <h1>${post.textoPost}</h1>
-        <button class="delete-btn">Deletar</button>
-      <button class="like-btn">Curtir</button>
-    `;
+  if (Array.isArray(posts)) {
+    posts.forEach(post => {
+      const postElement = document.createElement('div');
+      postElement.innerHTML = `
+        <h1>${post.textoPost}</h1>
+        <button class="delete-btn" data-postid="${post.id}">Deletar</button>
+        <button class="like-btn">Curtir</button>
+      `;
+      postArea.appendChild(postElement);
+    });
   }
-  
+
+  postArea.addEventListener('click', deletarPost);
+  async function deletarPost(event) {
+    const deleteButton = event.target;
+    if (deleteButton.classList.contains('delete-btn')) {
+      const postId = deleteButton.getAttribute('data-postid');
+      if (postId) {
+        await deletePost(postId);
+        deleteButton.parentElement.remove();
+      }
+    }
+  }
+
   return container;
 };
+
